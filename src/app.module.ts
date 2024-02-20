cb9311f41fd47d9050997459b7b681e53df2c91a
ysameh0 <y.sameh@eseed.net>
2024-02-21 00:46:00 +0200

    pg install, typeorm init, config service init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmOptions } from './config/options/typeorm.options';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    MessagesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync(TypeOrmOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/options/typeorm.options.ts
@@ -0,0 +1,17 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const TypeOrmOptions: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    synchronize: true,
+    autoLoadEntities: true,
+  }),
+  inject: [ConfigService],
+};
